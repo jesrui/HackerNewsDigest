da6c2b16a1eafd0fc8ef713f025e0ccf93b2f0a6
Jesus Ruiz <jesrui@gmail.com>
2014-07-07 14:46:14 +0200

    allow to expand / collapse comments
    javascript code taken from https://github.com/niyazpk/Collapsible-comments-for-Hacker-News

--- default.website/templates.lua
+++ default.website/templates.lua
@@ -6,6 +6,69 @@ return {
 <meta charset="utf-8"/>
 <meta name="viewport" content="width=device-width"/>
 <title>$title | Hacker News Digest</title>
+<script type="text/javascript" src="/js/jquery-1.7.2.min.js"></script>
+<script type="text/javascript">
+$(document).ready(function(){
+    if (typeof jQuery !== 'undefined') {
+        if (!$('body').hasClass('collapsible-comments')) {
+            $('body').addClass('collapsible-comments');
+            $('.expand-handle').live('click', function () {
+                current_level_width = parseInt($(this).closest('tr').find('td:eq(0) > img').attr('width'), 10);
+                $(this).closest('table').closest('tr').nextAll().each(function (index, el) {
+                    var elWidth = parseInt($('tbody > tr > td > img', this).attr('width'), 10);
+                    if (elWidth > current_level_width) {
+                        if (elWidth <= inner_level_width) {
+                            inner_level_width = 1000;
+                            $(this).hide()
+                        }
+                        if (inner_level_width == 1000 && $('.comment', this).css('display') == 'none') {
+                            inner_level_width = elWidth
+                        }
+                    } else {
+                        return false
+                    }
+                });
+                inner_level_width = 1000;
+                $(this).text('[+]').addClass('expand-handle-collapsed').removeClass('expand-handle');
+                $(this).closest('td').next().nextAll().hide();
+                $(this).closest('td').next().find('div').nextAll().hide();
+                $(this).closest('td').next().css({
+                    'margin-left': '18px',
+                    'margin-top': '2px',
+                    'margin-bottom': '5px'
+                })
+            });
+            $('.expand-handle-collapsed').live('click', function () {
+                current_level_width = parseInt($(this).closest('tr').find('td > img').attr('width'), 0);
+                $(this).closest('table').closest('tr').nextAll().each(function (index, el) {
+                    var elWidth = parseInt($('tbody > tr > td > img', this).attr('width'), 0);
+                    if (elWidth > current_level_width) {
+                        if (elWidth <= inner_level_width) {
+                            inner_level_width = 1000;
+                            $(this).show()
+                        }
+                        if (inner_level_width == 1000 && $('.comment', this).css('display') == 'none') {
+                            inner_level_width = elWidth
+                        }
+                    } else {
+                        return false
+                    }
+                });
+                inner_level_width = 1000;
+                $(this).text('[-]').addClass('expand-handle').removeClass('expand-handle-collapsed');
+                $(this).closest('td').next().nextAll().show();
+                $(this).closest('td').next().find('div').nextAll().show();
+                $(this).closest('td').next().css({
+                    'margin-left': '0',
+                    'margin-bottom': '-10px'
+                })
+            })
+        }
+    }
+    var current_level_width = 0;
+    var inner_level_width = 1000
+});
+</script>
 </head>
 ]==],
     comments_body_top = [==[
@@ -51,9 +114,13 @@ return {
                                 <table border=0>
                                     <tr>
                                         <td>
-                                            <img src="s.gif" height=1 width=$indentw>
+                                            <img src="/s.gif" height=1 width=$indentw>
                                         </td>
-                                        <td class="default">
+                                        <td valign=top>
+                                            <span style='cursor:pointer;margin-right:10px;' class='expand-handle'>[-]
+                                            </span>
+                                        </td>
+                                        <td class="default" valign=top>
                                             <div style="margin-top:2px; margin-bottom:-10px; ">
                                                 <span class="comhead">
                                                     <a href="https://news.ycombinator.com/user?id=$author">$author</a>
